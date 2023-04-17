6ecae4d40fbb6eb08c30cee1f1ee49ce990cc24f
DESKTOP-FOVJ9PU\Advance <usman.iqbal8899@gmail.com>
2023-04-17 16:00:15 -0700

    connect

---
+++ src/test/java/com/connectMySQLDBwithSpringBoot/ConnectMySqlDbWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectMySQLDBwithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectMySqlDbWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
